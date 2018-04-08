c35c279e33620d5b3fa1019d4c1e0f83b4bd8944
Arkadiy <arkadiy@ex.ua>
2018-04-08 10:12:05 +0300

    initial setup ver. 0.0.0

---
+++ include/sockets.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Sockets {
+public:
+    Sockets();
+
+};
+
+
+
---
+++ src/sockets.cpp
@@ -0,0 +1,12 @@
+#include <sockets.h>
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <resolv.h>
+#include <string>
+
+Sockets::Sockets() {
+
+}
